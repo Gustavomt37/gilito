5d0a40b812a5dce13c55800ab4f7fd623e692a7c
coco <gustavomt37@gmail.com>
2014-03-27 15:46:37 -0600

    guardar modificaciones

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Gilito::Application.config.secret_key_base = 'd0c82946a5d85668ca6524da90740eac52f4d7a28bff449c78937a37233dee12e41a3d1a7487c0ff7cab665a56cdb9e4b6d91a26a6793b4248e654f8e0ec8d74'
